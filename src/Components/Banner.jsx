d2f72baab1ea060f85d95a28315de932854fc99c
Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2024-03-26 10:46:28 +0600

    banner section complete

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <section className="bg-gray-800 text-gray-100 mt-10">
+        <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+            <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+                <h1 className="text-5xl font-bold leading-none sm:text-6xl">Books to freshen up your bookshelf 
+                </h1>
+               
+                <div className="mt-10 flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+                    <a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold rounded bg-violet-400 text-gray-900">View The List</a>
+                   
+                </div>
+            </div>
+            <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+                <img src="../../img/pngwing 1.png" alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
+            </div>
+        </div>
+    </section>
+    );
+};
+
+export default Banner;
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Navber from './Pageses/Navber.jsx'
+import Banner from './Components/Banner.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    
     <Navber></Navber>
+    <Banner></Banner>
   </React.StrictMode>,
 )
